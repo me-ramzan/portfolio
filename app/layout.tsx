48ee77369add5d30a0adaecbf7ca784fca980f6e
Muhammad Usman <me.ramzan.zulfiqar@gmail.com>
2025-10-28 01:58:40 +0500

    updated - 28/10/25

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { ThemeProvider } from "@/components/theme-provider"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "Ateeb Shahid | Personal Portfolio",
-  description: "Personal portfolio of Ateeb Shahid",
+  title: "Muhammad Ramzan | Personal Portfolio",
+  description: "Personal portfolio of Muhammad Ramzan",
   generator: 'v0.dev',
   icons: {
     icon: '/pfp-icon.ico',
